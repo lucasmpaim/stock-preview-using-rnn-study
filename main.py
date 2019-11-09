b8e3702fbae661eb29a1691e31f7a1660cc9e814
lucaspaim <lucasmpaim1@gmail.com>
2019-11-09 01:10:37 -0300

    remove unused code

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import datetime
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from tensorflow_core.python.keras.layers.core import Dense, Dropout
 from tensorflow_core.python.keras.layers.recurrent import LSTM
-from tensorflow_core.python.keras.utils.vis_utils import plot_model
 
 from utils.split_dataset import split_dataset
 from utils.data_frame_utils import rename_frame, normalize
@@ -25,39 +23,15 @@ dolar_frame = data_frame.iloc[1:, 9:11]
 rename_frame(dolar_frame)
 dolar_frame['Close'] = normalize(dolar_frame['Close'])
 
-TRAIN_SPLIT = 3000
-
-# plt.plot(range(0, ibov_frame.shape[0]),
-#          [y for y in ibov_frame['Close']],
-#          label='Ibov', alpha=0.5)
-#
-# plt.plot(range(0, dolar_frame.shape[0]),
-#          [y for y in dolar_frame['Close']],
-#          label='Dólar', alpha=0.5)
-#
-# plt.plot(range(0, petr4_frame.shape[0]),
-#          [y for y in petr4_frame['Close']],
-#          label='Petr4')
-#
-# plt.legend()
-# plt.xlabel('Days after 05/09/2006')
-# plt.ylabel('Normalized Variation')
-# plt.title('Petr4 x Dólar x Ibov')
-# plt.show()
-
 # configure seed's
 tf.random.set_seed(22)
 np.random.seed(22)
 
 features = pd.DataFrame()
-# features['Ibov'] = ibov_frame['Close'].to_numpy()
-# features['Dolar'] = dolar_frame['Close'].to_numpy()
 features['Petr4'] = petr4_frame['Close'].to_numpy()
 
 print(features.head())
 print(features.shape)
-# features.plot(subplots=True)
-# plt.show()
 
 tf.random.set_seed(13)
 
@@ -111,14 +85,4 @@ model.fit(X_train, y_train, epochs=EPOCHS,
           validation_data=tensor_dataset_slice_val,
           callbacks=[tensorboard_callback])
 
-# predicted = model.predict(X_val)
-# predicted2 = np.ravel(predicted)
-# y_test2 = np.ravel(y_test)
-
-# plt.plot(range(0, past_history), predicted2, '*', label='predict')
-# plt.plot(range(0, past_history), y_test2, 'x', label='real')
-# plt.legend()
-# plt.show()
-
-# differential_evolution_minimize
 model.save_weights('models/deep_weights.rnn')
--- predict.py
+++ predict.py
@@ -1,16 +1,13 @@
-import datetime
-
-import tensorflow as tf
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import tensorflow as tf
 from tabulate import tabulate
 from tensorflow_core.python.keras.layers.core import Dense, Dropout
 from tensorflow_core.python.keras.layers.recurrent import LSTM
-from tensorflow_core.python.keras.utils.vis_utils import plot_model
 
-from utils.split_dataset import split_dataset
 from utils.data_frame_utils import rename_frame, undo_normalize, normalize
+from utils.split_dataset import split_dataset
 
 data_frame = pd.read_csv('data/stocks.csv')
 
@@ -20,38 +17,15 @@ normalized = petr4_frame.copy()
 normalized['Close'] = normalize(normalized['Close'])
 
 TRAIN_SPLIT = 3000
-
-# plt.plot(range(0, ibov_frame.shape[0]),
-#          [y for y in ibov_frame['Close']],
-#          label='Ibov', alpha=0.5)
-#
-# plt.plot(range(0, dolar_frame.shape[0]),
-#          [y for y in dolar_frame['Close']],
-#          label='Dólar', alpha=0.5)
-#
-# plt.plot(range(0, petr4_frame.shape[0]),
-#          [y for y in petr4_frame['Close']],
-#          label='Petr4')
-#
-# plt.legend()
-# plt.xlabel('Days after 05/09/2006')
-# plt.ylabel('Normalized Variation')
-# plt.title('Petr4 x Dólar x Ibov')
-# plt.show()
-
 # configure seed's
 tf.random.set_seed(22)
 np.random.seed(22)
 
 features = pd.DataFrame()
-# features['Ibov'] = ibov_frame['Close'].to_numpy()
-# features['Dolar'] = dolar_frame['Close'].to_numpy()
 features['Petr4'] = normalized['Close'].to_numpy()
 
 print(features.head())
 print(features.shape)
-# features.plot(subplots=True)
-# plt.show()
 
 tf.random.set_seed(13)
 
